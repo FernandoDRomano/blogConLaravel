b9155183da1221ccb59f07f1d9baf7abce281c87
Fernando Daniel Romano <fernandoreceptor2014@gmail.com>
2020-11-08 22:50:20 -0300

    Agregando las migraciones

---
+++ database/migrations/2020_11_09_005549_create_posts_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->mediumText('extract')->nullable();
+            $table->text('body')->nullable();
+            $table->mediumText('iframe')->nullable();
+            $table->date('published_at')->nullable();
+            $table->boolean('approved')->default(false);
+            $table->string('url')->unique();
+            /* LLAVES FORANEAS */
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->unsignedBigInteger('category_id')->nullable();
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('set null');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
